45ed2e63f7d38eb708917df06347d15d7e0d6135
pulkit <pulkitgpt1234@gmail.com>
2021-04-16 09:57:36 +0530

    project commit

---
+++ app/src/main/java/com/example/socially/daos/UserDao.kt
@@ -0,0 +1,28 @@
+package com.example.socially.daos
+
+
+import com.example.socially.models.User
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class UserDao {
+    private val db = FirebaseFirestore.getInstance()
+    private val usersCollection = db.collection("users")
+
+    //adds data to firebase users colection
+    fun addUser(user: User?) {
+        user?.let {
+            GlobalScope.launch(Dispatchers.IO) {
+                usersCollection.document(user.uid).set(it)
+            }
+        }
+    }
+
+    fun getUserById(uID:String): Task<DocumentSnapshot> {
+        return usersCollection.document(uID).get()
+    }
+}
